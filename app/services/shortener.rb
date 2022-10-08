e2ab37c9bac5145453631c572fefcd2bedc45030
dylan <dyan2009200@gmail.com>
2022-10-09 02:02:52 +0800

    add links_controller

--- app/services/shortener.rb
+++ app/services/shortener.rb
@@ -13,23 +13,16 @@ class Shortener
   end
 
   def lookup_code
-    # 1. get a lookup code
-    # 2. check ig it already exist
-    # 3. break out of the loop if it does
-    # 4. if not, get another code
-
-    i = 0 
     loop do 
-      code = get_fresh_code(i)
+      code = get_fresh_code
       break code unless link_model.exists?(lookup_code: code)
-      i = i + 1
     end
   end
 
   private
 
-  def get_fresh_code(i)
-    SecureRandom.uuid[i ..(i + 7)]
+  def get_fresh_code
+    SecureRandom.uuid[0..7]
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
 
   root to: 'home#index'
   resources :urls
-  
+  # resources :links
+  post "/links", to: "links#create"
 end
